adb4ae1f9c2606e28e5d2fb43c026f85627b7fdc
Alexis Montes <montes.alexis25@gmail.com>
2021-07-22 00:55:02 -0400

    changed order of routes

--- myApp.js
+++ myApp.js
@@ -3,14 +3,16 @@ var app = express();
 
 console.log("Hello World");
 
-//app.use(express.static(__dirname + '/public'));
-
-app.use("/public", express.static(__dirname + "/public"));
-
 app.use((req,res,next) => {
     console.log(req.method + " " + req.path + " " + req.ip);
     next();
 });
+
+//app.use(express.static(__dirname + '/public'));
+
+app.use("/public", express.static(__dirname + "/public"));
+
+
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
