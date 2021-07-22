06f60b674a15bc0aaa1345a0268ef1c5a15e129e
Alexis Montes <montes.alexis25@gmail.com>
2021-07-22 01:33:11 -0400

    added name route

--- myApp.js
+++ myApp.js
@@ -45,6 +45,12 @@ app.get("/:word/echo", function(req, res) {
     });
 })
 
+app.get("/name", function(req, res) {
+    res.json({
+        "name": req.query.first + " " + req.query.last
+    });
+})
+
 
 
 
